c5baaa9cfec3d10052973e2213c3ac92266f1c2c
Helder Pereira <hplive@hotmail.com>
2021-10-21 21:47:06 +0100

    bio changed

--- src/components/About/index.js
+++ src/components/About/index.js
@@ -5,26 +5,14 @@ import './style.css'
 const About = () => {
     return (
         <div className="about">
-            <h1>Bio</h1>
-            <p>Hi! I'm <strong>Helder Pereira</strong> </p>
-            <p>Later i will talk a litle more of my self. But for now, just saying i love the chalenge of developing. I just love to see something i builded, or builded with my colegues, online or in production.</p>
-            <p>A litle more of my self...later. now i'm under construction :) and so all this site :)</p>
-           {/* <p>
-            I'm <strong>Helder Pereira</strong>. I like to develop and recently i can enjoy the freedom to develop what i want.
-             <br></br>
-            A long time ago, in a galaxy far far away, I gave up from studying, and I started to work at full time in the retail store.<br></br>
-            After that i've been away from pc's for some while... for so much time. At some point, one retail supervisor chalenge me to make some simple app to clean tables from emails...i did it. After that, the stored manager saw that, and chalenge me to make an app able to manage some PDF's. Challenge acepted. I've builded in java...after that, I converted everything to PHP with a database online and then upgraded with a bunch of new things and functionalities. <br></br>
-            Then someone challenge me if I could make an app for windows phone of a gun fireing...Made it that, and publish it to Windows Phone Store, and some more app's for Windows Phone/Mobile. 
-            
-            Windows Mobile :( ...I realy thouth it was going to be the only mobile operating system at some point in the future...i was so wrong.<br></br>
-            The love for developing was coming...but i couldn't go back to college...not yet. The life wasn't going as i intended...i had to conform. That was going to be my life. Until, the best thing happened meanwhile, and i had so much more time for myself, to invest in me, and to live. Soon i've starded surfing, and surfing every day i went, sometimes even twice a day. And then... something hit me again. I have so much free time, i cant just surf for the rest of my life! i i stink in surf :). I have to go back to college, and clever thouth i had. So i went, and it was the best decision i had in years.<br></br>
-            I've met there the 3 best palls that someone could ask. Giving up will never be an option anymore thanks to them. <br></br>
-            Meanwhile, ii was time to leave my comfort zone and went to find for a job in my studing area. Fotunately, i've found in the same company, so it was so much easier the change.<br></br>
-            I've found a team with an awesome boss, and the mates of those team where also awesome, the best. The team now is dissipating, but the heart of team is for ever.<br></br>
-            Now recently i've finish the f**** graduation and the world is just opening the doors for me.
+            <h1>Professional Bio</h1>
+            <p>I am <strong>Helder Pereira</strong> </p>
+            <p>A Computer Enginner in Porto, with a passion for computer science,specialy web and mobile development</p>
+            <p>Joined in May 2018, to an IT team in Sonae, i've been developing .net framework and .net core for a project, more backend, and with some front-end with javascript, and the old Jquery(not so much any more).</p>
+            <p>Recently, finish my degree in Computer Science in Instituto Superior de Engenharia do Porto, and now my challenge is to learn as much as i can 
+            about React and React-native, so i can do the thing i enjoy more. To publish app to app stores</p>
             <br></br>
-            <strong>So...lets see what is inside :)</strong> 
-            </p>*/}
+            <strong>Now there is an open door for the world, so...lets see what's inside :)</strong> 
             <br/>
            
         </div>
